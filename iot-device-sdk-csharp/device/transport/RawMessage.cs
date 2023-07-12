9daf88ba2267a1c66290dc7e19704c301fd0cf35
Aimini <2026084033@qq.com>
2023-07-12 16:08:13 +0800

    Dev (#10)
    * add copyright and new features
    
    * add copyright and new features

--- iot-device-sdk-csharp/device/transport/RawMessage.cs
+++ iot-device-sdk-csharp/device/transport/RawMessage.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2020-2020 Huawei Cloud Computing Technology Co., Ltd. All rights reserved.
+ * Copyright (c) 2020-2023 Huawei Cloud Computing Technology Co., Ltd. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -27,6 +27,8 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+using System.Text;
 
 namespace IoT.SDK.Device.Transport
 {
@@ -85,10 +87,19 @@ namespace IoT.SDK.Device.Transport
         public string Topic { get; set; }
 
         /// <summary>
+        /// Indicates a message body in binary format.
+        /// </summary>
+        public byte[] BinPayload { get; set; }
+
+        /// <summary>
         /// Indicates a message body.
         /// </summary>
-        public string Payload { get; set; }
-
+        public string Payload
+        {
+            get => Encoding.UTF8.GetString(BinPayload);
+            set => BinPayload = Encoding.UTF8.GetBytes(value);
+        }
+    
         /// <summary>
         /// Indicates a QoS level. The value can be 0 or 1. The default value is 1.
         /// </summary>
